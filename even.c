fcbd38d4a8f8209fcec058caf473c9da24f83ba6
Vasanth57 <30317765+Vasanth57@users.noreply.github.com>
2017-07-22 17:29:10 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#inlude<conio.h>
+void main()
+{
+int n;
+printf("enter the number")l;
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("the number is even");
+}
+else
+{
+printf("the number is odd");
+}
+}
